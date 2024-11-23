2ab4e58f861c1d1719463f73a57756c4be407ae8
Yeqin Gong <gon@gyeq.in>
2024-11-23 16:05:04 +0800

    update

--- src/generators/python/events.js
+++ src/generators/python/events.js
@@ -2,6 +2,6 @@ import { pythonGenerator } from './generator';
 
 pythonGenerator['event_whenkeypressed'] = (block) => {
   const keyCode = block.getFieldValue('KEY_OPTION');
-  const branchCode = pythonGenerator.eventToCode('key_pressed');
+  const branchCode = pythonGenerator.eventToCode('key_pressed', 'False');
   return `@when_keypressed("${keyCode}")\n${branchCode}`;
 };
